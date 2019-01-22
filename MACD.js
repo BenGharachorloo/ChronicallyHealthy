dbc26dea012aec0b3ee11b1897ffc91943e78f49
BenGharachorloo <ben.gharachorloo@gmail.com>
2019-01-21 22:55:16 -0500

    Created test MACD script

---
+++ MACD.js
@@ -0,0 +1,18 @@
+function MACD(weightVector) {
+ 
+ var result = weightVector[1] - weightVector[0]
+ 
+ return result;
+}
+
+var threshold = 1;
+
+var weightVector = [70,70,70,70,70,70,70,70,70, 70.5, 71, 72, 73.1]; //will replace with weight data from scale
+
+//MACD
+
+var result = MACD(weightVector);
+
+if (result > threshold)
+  //alert
+  
