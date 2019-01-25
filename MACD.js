ebf94d7223513f7875d5051258afddbd4616f6c6
BenGharachorloo <ben.gharachorloo@gmail.com>
2019-01-25 16:56:44 -0500

    testing MACD

--- MACD.js
+++ MACD.js
@@ -31,15 +31,39 @@ var OI = alphS*shortSum - alphL*longSum;
 
 var threshold = 0.4;
 
-var weightVector = [50,70,70,70,70,70,70,70,70, 70.5, 71, 72, 73.1]; //will replace with weight data from scale
+var weightVector = [70,70,70,70,70,70,70,70,70,70,70,70,70.6,70.9,71,71.6,71.8,72.3]; //will replace with weight data from scale
 
 //MACD
 document.write("CALCULATING WEIGHT TRENDS.");
 
+
+for(int i = 0; i < 4; i ++){
+<br>
+<br>
+document.write("*************");
 var result = MACD(weightVector);
+document.write("Weight trend is: ");
+document.write(weightVector);
+<br>
+<br>
+document.write("OI Result is: ");
+document.write(result);
+
+<br>
+<br>
 
 if (result - 1 > threshold)
   document.write("UH OH! Something is wrong! Please see a doctor.");
 else
   document.write("You're safe.");
-  
+	if(i == 0)
+		weightVector =  [70,70,70,70,70,70,70,70,70,70,70,70,70]; 
+	if(i == 1)
+		weightVector =  [70,70,70,70,71,70,68,70,70,70,70,71,70.1]; 
+	if(i == 2)
+		weightVector =  [70,70,70,70,70,70,70,70,70,68.5,70,69,70.1]; 
+	if(i == 3)
+		weightVector =  [70,70,70,70,70,70,70,70,70,68.5,70,72,75.1]; 
+	
+	weightVector =  [70,70,70,70,70,70,70,70,70,70,70,70,70]; 
+}
